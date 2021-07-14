df992f98216527a0e27f98d106b33fd2587897a2
vandnaparashar <85275087+vandnaparashar@users.noreply.github.com>
2021-07-14 10:26:12 +0530

    updated double locators and done the changing in the code

--- features/pageobjects/register.page.ts
+++ features/pageobjects/register.page.ts
@@ -1,32 +1,34 @@
-import Page from "./page";
+import Page from '../pageobjects/page'
 
-class appData extends Page{
+
+class appData extends Page {
 
     get header() { return $("//h1"); }
-    get fname() { return $('//input[@name="firstname"]'); }
-    get lname() { return $('//input[@name="lastname"]'); }
-    get gender_ratio () { return $$('//input[@name="sex"]'); }
-    get experience() { return $$('//input[@name="exp"]'); }
-    get date() { return $('//input[@id="datepicker"]'); }
+    get fname() { return $('//input[@name="firstname"]') }
+    get lname() { return $('//input[@name="lastname"]') }
+    get gender_radio() { return $$('//input[@name="sex"]') }
+    get experience() { return $$('//input[@name="exp"]') }
+    get date() { return $('//input[@id="datepicker"]') }
     get favouritechai_checkbox() { return $$('//input[@type="checkbox"]') }
-    get  excitingchai_checkbox () { return $$('//input[@name="tool"]')}
-    get continent_dropdown() { return $('//select [@id="continents"]'); }
-    get selcommands() { return $('//select[@id="selenium_commands"]'); }
+    get excitingchai_checkbox() { return $$('//input[@name="tool"]') }
+    get continent_dropdown() { return $('//select [@id="continents"]') }
+    get selcommands() { return $('//select[@id="selenium_commands"]') }
     get submit_btn() { return $('//div[@class="button"]'); }
+    
 
-    async selectdropdown(element: WebdriverIO.ElementArray, value:string) {
+    async selectdropdown(element: WebdriverIO.ElementArray, value: string) {
         for (let i = 0; i < element.length; i++) {
             const elm = await (element[i]).getAttribute('value');
             if (elm == value) {
-             await element[i].click()
-             break;
+                await (element[i]).click()
+                break;
+            }
         }
     }
-} 
 
-//   async enterfirstname(firstname:string)
-//     await (await this.fname.setValue()
-    
+    //   async enterfirstname(firstname:string)
+    //     await (await this.fname.setValue()
+
 }
 
 export default new appData()
--- features/register.feature
+++ features/register.feature
@@ -4,12 +4,12 @@ Feature: chai page
         Given I am on practice page "http://www.practiceselenium.com/practice-form.html"
         Then I validate page header "Practice Form"
         When I enter firstname <fname> and lastname <lname>
-        And I select gender <gender> years <yrs> favourite chai <favchai> and reason <reason>
-        I select gender (.+) years  (.+) favourite chai (.+) and reason (.+)
+        And I select gender <gender> years <yrs> date <date> favourite chai <favchai> and reason <reason>
         And I select continent <continent> and commands <command>
         And I click on submit button
-        Then I validate page header "We're passionate about tea."
+        
 
         Examples:
-            | fname  | lname    | gender | yrs | date |favchai | reason             | continent | command       |
-            | vandna | parashar | Female | 8   | 08-09-2019 |Red Tea | Harmless Addiction | Europe    | Wait Commands |
+            | fname  | lname    | gender | yrs | date       | favchai | reason             | continent | command       |
+            | vandna | parashar | Female | 5  | 08-09-2019 | Red Tea | Harmless Addiction | Europe    | Wait Commands |  
+
